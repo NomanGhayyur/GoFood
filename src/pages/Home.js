b6828e332d169eec5076ba1f4a843009368318c5
NomanGhayyur <nomanghayyur96@gmail.com>
2023-02-16 18:13:27 +0500

    Created Folder Structer with routs

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <Navbar />
+        Body
+        <Footer />
+      </div>
+    </>
+  );
+}
